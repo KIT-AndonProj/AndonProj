93a836288a62d6e482b51896b61cc0a5313dc74a
crsherbet <warisaracr.y@gmail.com>
2018-06-26 12:34:29 +0700

    implement repoinfo api

--- routes/api/gitInfo.js
+++ routes/api/gitInfo.js
@@ -10,34 +10,35 @@ const options = {
     }
 };
 
-router.get('/testA',(req, res) => {
-    options.url = options.url + 'repos/crsherbet/memonkeyproj/commits' 
-    const callback = (error, response, body) => {
-        if(error){ return res.json(error) }
-        const result = []
-        const bodyJson = JSON.parse(body)
-        var dateCheck = '';
-        for( i = bodyJson.length-1; i >= 0; i-- ){
-            if(dateCheck === '' || dateCheck !== bodyJson[i].commit.author.date.slice(0,10)){
-                dateCheck = bodyJson[i].commit.author.date.slice(0,10)
-                result.push({name: dateCheck, commit: 1})
-            } else { 
-                dateCheck = bodyJson[i].commit.author.date.slice(0,10)
-                result[result.length-1].commit += 1;
-            }
-        }
-        return res.json(result)
-    }
+// router.get('/testA',(req, res) => {
+//     options.url = options.url + 'repos/crsherbet/memonkeyproj/commits' 
+//     const callback = (error, response, body) => {
+//         if(error){ return res.json(error) }
+//         const result = []
+//         const bodyJson = JSON.parse(body)
+//         var dateCheck = '';
+//         for( i = bodyJson.length-1; i >= 0; i-- ){
+//             if(dateCheck === '' || dateCheck !== bodyJson[i].commit.author.date.slice(0,10)){
+//                 dateCheck = bodyJson[i].commit.author.date.slice(0,10)
+//                 result.push({name: dateCheck, commit: 1})
+//             } else { 
+//                 dateCheck = bodyJson[i].commit.author.date.slice(0,10)
+//                 result[result.length-1].commit += 1;
+//             }
+//         }
+//         return res.json(result)
+//     }
        
-    Request(options, callback);
-}) 
+//     Request(options, callback);
+// }) 
 
 router.post('/commits', (req, res) => {
     options.url = options.url + 'repos/' + req.body.username + '/' + req.body.repository + '/commits' 
+    var result = {};
+    var bodyJson = {};
     const callback = (error, response, body) => {
         if(error){ return res.json(error) }
-        const result = []
-        const bodyJson = JSON.parse(body)
+        bodyJson = JSON.parse(body)
         var dateCheck = '';
         for( i = bodyJson.length-1; i >= 0; i-- ){
             if(dateCheck === '' || dateCheck !== bodyJson[i].commit.author.date.slice(0,10)){
@@ -54,12 +55,32 @@ router.post('/commits', (req, res) => {
     Request(options, callback);
 })
  
+router.post('/repoinfo', (req, res) => {
+    options.url = options.url + 'repos/' + req.body.username + '/' + req.body.repository
+    var bodyJson = {};
+    var result = {};
+    const callback = (error, response, body) => {
+        if(error){ return res.json(error) }
+        bodyJson = JSON.parse(body);
+        result = {
+            username: bodyJson.owner.login,
+            reponame: bodyJson.name,
+            created_at: bodyJson.created_at,
+            updated_at: bodyJson.updated_at,
+            pushed_at: bodyJson.pushed_at,
+            num_issue: bodyJson.open_issues,
+            image_url: bodyJson.owner.avatar_url
+        }
+        return res.json(result)
+    }
+       
+    Request(options, callback);
+});
+
 router.post('/info', (req, res) => {
     options.url = options.url + 'users/' + req.body.username
     const callback = (error, response, body) => {
-        if(error){
-            return res.json(error)
-        }
+        if(error){ return res.json(error) }
         return res.json(body)
     }
        
