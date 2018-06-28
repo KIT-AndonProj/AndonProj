17c37ac307ee880c56be935dc317df152c12efde
crsherbet <warisaracr.y@gmail.com>
2018-06-28 13:28:13 +0700

    implement git api and use axios instead of request library

--- routes/api/gitInfo.js
+++ routes/api/gitInfo.js
@@ -2,101 +2,73 @@ const express = require('express');
 const router = express.Router(); 
 const Request = require('request')
 const User = require('../../models/User');
-
-const options = {
-    url: `https://api.github.com/`,
-    headers: {
-      'User-Agent': 'AndonProj'
-    }
-};
-
-// router.get('/testA',(req, res) => {
-//     options.url = options.url + 'repos/crsherbet/memonkeyproj/commits' 
-//     const callback = (error, response, body) => {
-//         if(error){ return res.json(error) }
-//         const result = []
-//         const bodyJson = JSON.parse(body)
-//         var dateCheck = '';
-//         for( i = bodyJson.length-1; i >= 0; i-- ){
-//             if(dateCheck === '' || dateCheck !== bodyJson[i].commit.author.date.slice(0,10)){
-//                 dateCheck = bodyJson[i].commit.author.date.slice(0,10)
-//                 result.push({name: dateCheck, commit: 1})
-//             } else { 
-//                 dateCheck = bodyJson[i].commit.author.date.slice(0,10)
-//                 result[result.length-1].commit += 1;
-//             }
-//         }
-//         return res.json(result)
-//     }
-       
-//     Request(options, callback);
-// }) 
+const axios = require('axios');
 
 router.post('/commits', (req, res) => {
-    options.url = options.url + 'repos/' + req.body.username + '/' + req.body.repository + '/commits' 
-    var result = {};
-    var bodyJson = {};
-    const callback = (error, response, body) => {
-        if(error){ return res.json(error) }
-        bodyJson = JSON.parse(body)
+    url = 'https://api.github.com/repos/' + req.body.username + '/' + req.body.repository + '/commits' 
+    var result = [];
+   
+    axios.get(url).then(response => { 
         var dateCheck = '';
-        for( i = bodyJson.length-1; i >= 0; i-- ){
-            if(dateCheck === '' || dateCheck !== bodyJson[i].commit.author.date.slice(0,10)){
-                dateCheck = bodyJson[i].commit.author.date.slice(0,10)
+        for( i = response.data.length-1; i >= 0; i-- ){
+            if(dateCheck === '' || dateCheck !== response.data[i].commit.author.date.slice(0,10)){
+                dateCheck = response.data[i].commit.author.date.slice(0,10)
                 result.push({name: dateCheck, commit: 1})
             } else { 
-                dateCheck = bodyJson[i].commit.author.date.slice(0,10)
+                dateCheck = response.data[i].commit.author.date.slice(0,10)
                 result[result.length-1].commit += 1;
             }
         }
         return res.json(result)
-    }
-       
-    Request(options, callback);
+    }).catch(err => console.log(err))
 })
- 
+
+router.post('/branches', (req, res) => {
+    url = 'https://api.github.com/repos/' + req.body.username + '/' + req.body.repository + '/branches' 
+    var result = [];
+
+    axios.get(url).then( response => {
+        for( i in response.data){
+            result.push(
+                {   name: response.data[i].name, sha: response.data[i].commit.sha,
+                    lastcommit_url: 'https://github.com/' + req.body.username + '/' + req.body.repository + '/commit/' + response.data[i].commit.sha
+                }
+            );
+        } return res.json(result)
+    }).catch(err => console.log(err))
+})
+
 router.post('/repoinfo', (req, res) => {
-    options.url = options.url + 'repos/' + req.body.username + '/' + req.body.repository
-    var bodyJson = {};
-    var result = {};
-    const callback = (error, response, body) => {
-        if(error){ return res.json(error) }
-        bodyJson = JSON.parse(body);
+    url = 'https://api.github.com/repos/' + req.body.username + '/' + req.body.repository  
+   
+    axios.get(url).then(response => { 
         result = {
-            username: bodyJson.owner.login,
-            reponame: bodyJson.name,
-            created_at: bodyJson.created_at,
-            updated_at: bodyJson.updated_at,
-            pushed_at: bodyJson.pushed_at,
-            num_issue: bodyJson.open_issues,
-            image_url: bodyJson.owner.avatar_url
+            username: response.data.owner.login,
+            reponame: response.data.name,
+            created_at: response.data.created_at,
+            updated_at: response.data.updated_at,
+            pushed_at: response.data.pushed_at,
+            num_issue: response.data.open_issues,
+            image_url: response.data.owner.avatar_url
         }
         return res.json(result)
-    }
-       
-    Request(options, callback);
+    }).catch(err => console.log(err))
 });
 
 router.post('/info', (req, res) => {
-    options.url = options.url + 'users/' + req.body.username
-    const callback = (error, response, body) => {
-        if(error){ return res.json(error) }
-        return res.json(body)
-    }
-       
-    Request(options, callback);
+    url = 'https://api.github.com/users/' + req.body.username
+
+    axios.get(url).then(response => { 
+        return res.json(response.data)
+    }).catch(err => console.log(err))
 });
 
 router.post('/repolist', (req, res) => {
-    options.url = options.url + 'users/' + req.body.username + '/repos'
-    const callback = (error, response, body) => {
-        if(error){
-            return res.json(error)
-        }
-        return res.json(body)
-    }
-   
-    Request(options, callback);
+    url = 'https://api.github.com/users/' + req.body.username + '/repos'
+
+    axios.get(url).then(response => {
+        return res.json(response.data)
+    }).catch(err => console.log(err))
 });
 
 module.exports = router;
