b7fb493884bf861130a3d3d34f979e5a946b0304
crsherbet <warisaracr.y@gmail.com>
2018-06-30 17:45:41 +0700

    debug bugspot on board

--- routes/api/bugspot.js
+++ routes/api/bugspot.js
@@ -9,8 +9,12 @@ router.post('/analyze', (req, res) => {
         console.log('Clear');
         exec('cd routes/api && git clone https://github.com/'+ req.body.username +'/'+ req.body.repository+'.git code', (err,stdout,stderr) => { 
             console.log('Clone')
+            console.log(err)
+            console.log(stdout)
+            console.log(stderr)
             if (err) { return res.json(err) }
             exec('cd routes/api/code && bugspots' ,(err2, stdout2, stderr2) => {
+                console.log('bug')
                 if (err2) { return res.json(stdout2.slice(0,stdout2.length-1)) }
                 return res.json(stdout2)
             });
