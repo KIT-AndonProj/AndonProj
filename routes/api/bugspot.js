88fc26d1ab9ac32e073d4bb34d80c1276512aecf
crsherbet <warisaracr.y@gmail.com>
2018-06-30 17:37:26 +0700

    fix bugspot api

--- routes/api/bugspot.js
+++ routes/api/bugspot.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const router = express.Router(); 
 const { exec } = require('child_process');
-// const Git = require('nodegit')
-// const scanner = require('@berries/acai');
-// const fs = require('fs-extra');
+const fs = require('fs-extra');
 
 router.post('/analyze', (req, res) => { 
-
-    exec('git clone https://github.com/'+ req.body.username +'/'+ req.body.repository+'.git srccode', (err,stdout,stderr) => {
-        if (err) { return res.json(err) }
-        console.log('gitclone')
-        exec('cd srccode' + req.body.repository ,(err1, stdout1, stderr1) => {
-            if (err1) { return res.json(err1) }
-            console.log(stderr1)
-            exec('bugspots' ,(err2, stdout2, stderr2) => {
-                if (err2) { return res.json(err2) }
-                return res.json(stdout2) 
+    fs.remove(__dirname + '/code', (error) => {
+        if (error) { throw error; }
+        console.log('Clear');
+        exec('cd routes/api && git clone https://github.com/'+ req.body.username +'/'+ req.body.repository+'.git code', (err,stdout,stderr) => { 
+            if (err) { return res.json(stderr) }
+            exec('cd routes/api/code && bugspots' ,(err2, stdout2, stderr2) => {
+                if (err2) { return res.json(stdout2.slice(0,stdout2.length-1)) }
+                return res.json(stdout2)
             });
         });
-    });
+    })
 })
 
 module.exports = router;
