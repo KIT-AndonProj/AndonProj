b15968ee8966bbd711fc2d0b1f87a83cfbcbef3f
crsherbet <warisaracr.y@gmail.com>
2018-06-30 19:01:52 +0700

    fix bugspots api

--- routes/api/bugspot.js
+++ routes/api/bugspot.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router(); 
-const { exec } = require('child_process');
+const { exec , execSync }  = require('child_process');
 const fs = require('fs-extra');
 
 router.post('/analyze', (req, res) => { 
@@ -9,16 +9,11 @@ router.post('/analyze', (req, res) => {
         console.log('Clear');
         exec('cd routes/api && git clone https://github.com/'+ req.body.username +'/'+ req.body.repository+'.git code', (err,stdout,stderr) => { 
             console.log('Clone')
-            console.log(err)
-            console.log(stdout)
-            console.log(stderr)
             if (err) { return res.json(err) }
-            exec('cd routes/api/code && bugspots' ,(err2, stdout2, stderr2) => {
-                console.log('bug')
-                if (err2) { return res.json(stdout2.slice(0,stdout2.length-1)) }
-                return res.json(stdout2)
-            });
+            var result = execSync('cd routes/api/code && bugspots');
+            return res.json(result.toString('utf8'))
         });
+        
     })
 })
 
