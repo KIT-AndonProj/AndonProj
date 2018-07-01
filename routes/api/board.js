c1e33a04ec316f2111831cdf35b2c2252057dfaf
crsherbet <warisaracr.y@gmail.com>
2018-07-01 16:16:06 +0700

    move file to pythonScript

--- routes/api/board.js
+++ routes/api/board.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { exec , execSync }  = require('child_process');
 
 router.post('/welcome', (req, res) => { 
-    exec('sudo PYTHONPATH=".:build/lib.linux-armv7l-2.7" python pythonScript/rpi_ws281x/python/examples/welcomeBlink.py -c', (err,stdout,stderr) => { 
+    exec('sudo PYTHONPATH=".:build/lib.linux-armv7l-2.7" python pythonScript/welcomeBlink.py -c', (err,stdout,stderr) => { 
         console.log('Welcome');
 
         if (err) { return res.json(stderr) }
