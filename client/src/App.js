09eca8660661e18a9cc0835711965197d1731ef5
littlenune <littlenune@gmail.com>
2018-07-28 14:42:43 +0900

    Remove unused file

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-import './App.css';
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Monitor from './pages/Monitor';
-import Camera from './pages/Camera';
 
 class App extends Component {
   
